f187fb3a62c7524a6b75bebf917128fc462e7bd5
Deias <saied_am@hotmail.com>
2022-12-20 10:16:11 +0100

    feat: adding Comment model.
    The Comment model uses ContentType so we can allow a model to be related to any number of models by just adding three attributes to a Model:
    A ForeignKey field that points to a ContentType. Normally this is called content_type
    A PositiveIntegerField that stores the primary key of the related object. Normally this is called object_id
    A GenericForeignKey field, a special type of field that will look up the object from the other two new fields.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 class Tag(models.Model):
     """Tag model"""
@@ -13,6 +16,16 @@ class Tag(models.Model):
     class Meta:
         verbose_name = "Tag"
         verbose_name_plural = "Tags"
+
+
+class Comment(models.Model):
+    """Comment model."""
+
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
 
 
 class Post(models.Model):
