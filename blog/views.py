6085500ac74e44b6d2dd412a170ad6cc1bb47e99
Deias <saied_am@hotmail.com>
2022-12-20 12:08:01 +0100

    feat: adding index view to posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils import timezone
+from blog.models import Post
+
+
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
